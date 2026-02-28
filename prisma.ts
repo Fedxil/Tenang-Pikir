8c79639c5d70b6cd568d791be36524fe0522b3a4
Fedxil <nicholaschandra88@gmail.com>
2026-02-28 15:41:59 +0700

    update prisma.ts

--- prisma.ts
+++ prisma.ts
@@ -5,7 +5,9 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+const adapter = new PrismaPg({ 
+  connectionString: process.env.DATABASE_URL + "?pgbouncer=true&connection_limit=1"
+});
 
 export const prisma =
   globalForPrisma.prisma ?? new PrismaClient({ adapter });
